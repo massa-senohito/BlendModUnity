00cfe85e9ad212bad693de48ee13790b842f4318
massa-senohito <i7da.masa@gmail.com>
2020-03-25 08:56:14 +0900

    Blenderのwarpモディファイアを実装

---
+++ Assets/Scenes/Warp/WarpMod.cs
@@ -0,0 +1,238 @@
+﻿//using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Unity.Mathematics.math;
+using Matrix = UnityEngine.Matrix4x4;
+
+public class WarpMod : EntityBase
+{
+    float SQUARE( float x )
+    {
+        return x * x;
+    }
+
+    Matrix blend_m4_m4m4( Matrix src , Matrix dst , float srcweight )
+    {
+
+        //blend_m4_m4m4(tmat, mat_unit, mat_final, fallOff);
+        src.GetTRS( out var dLoc , out var dRot , out var dScale );
+        dst.GetTRS( out var sLoc , out var sRot , out var sScale );
+        // normされてるのでは
+        //mat3_normalized_to_quat
+
+        var fLoc  = lerp(dLoc,sLoc,srcweight);
+        var fQuat = slerp(dRot,sRot,srcweight);
+        RarePrint( $"fQuat : {fQuat}" );
+        var fSca  = lerp(dScale,sScale,srcweight);
+        var tmp = Matrix.TRS(fLoc,fQuat , fSca);
+        return tmp;
+    }
+    Matrix mul_m4_m4(Matrix m1 ,Matrix m2)
+    {
+        return m1 * m2;
+    }
+    [SerializeField]
+    float falloff_radius = 1.0f;
+
+    [SerializeField]
+    Transform from;
+
+    [SerializeField]
+    Transform to;
+
+    [SerializeField]
+    float strength;
+
+    Mesh mesh;
+    Vector3[] OrgVertice;
+
+    [SerializeField]
+    AnimationCurve fall;
+    enum WarpFlag
+    {
+        None,
+        MOD_WARP_VOLUME_PRESERVE,
+    }
+    [SerializeField]
+    WarpFlag flag;
+
+    enum WarpFallOff
+    {
+        Curve,
+        Sharp,
+        Smooth,
+    }
+    [SerializeField]
+    WarpFallOff fallOff;
+    float CulcFall(float fac)
+    {
+        var tmp = 1.0f;
+        switch ( fallOff )
+        {
+            case WarpFallOff.Curve:
+                tmp = fall.Evaluate( fac );
+                break;
+            case WarpFallOff.Sharp:
+                tmp = fac * fac;
+                break;
+            case WarpFallOff.Smooth:
+                tmp = 3.0f * fac * fac - 2.0f * fac * fac * fac;
+                break;
+            default:
+                break;
+        }
+        return tmp;
+    }
+
+    void RarePrint( string log )
+    {
+        if ( Random.Range( 0 , 10000 ) == 0 )
+        {
+            print( log );
+        }
+    }
+
+    List<Vector3> warpModifier_do( )
+    {
+        //var ob = GameMesh;
+        var fallOff_rad_sq = SQUARE(falloff_radius);
+        var fac = 1.0f;
+        // MOD_get_vgroup:
+        var obInv = transform.worldToLocalMatrix;
+        // 191付近
+        // fromをobjローカルの位置へ移動させる
+        var mat_from  =  mul_m4_m4( obInv , from.localToWorldMatrix);
+        var mat_to    =  mul_m4_m4( obInv , to.localToWorldMatrix);
+        // (AB)^-1 = B^-1 A^-1
+        // fromローカルに移動させ、オブジェクトのワールドに持っていく
+        var tmat      = mat_from.inverse;
+        // toをobjローカル
+        var mat_final = mul_m4_m4(tmat , mat_to);
+        var mat_from_inv = mat_from.inverse;
+        if ( strength < 0.0f )
+        {
+            strength = -strength;
+            var loc = mat_final.GetPosition();
+            mat_final = mat_final.inverse;
+            //mat_final.location -= loc;
+            mat_final.SetTRS(
+                mat_final.GetPosition( ) - loc ,
+                mat_final.GetRotation( ) ,
+                mat_final.GetScale( ) );
+        }
+        var weight = strength;
+        var numVerts = mesh.vertices.Length;
+        var temp = new List<Vector3>(numVerts);
+
+        var verts = OrgVertice;
+
+        for ( int i = 0 ; i < numVerts ; i++ )
+        {
+            var co = verts[i];
+            var oriCo = co;
+            //RarePrint( $"93 oriCo {oriCo} ; co {co}" );
+            fac = ( co - mat_from.GetPosition( ) ).sqrMagnitude;
+            // 0.007 ~ 0.0168
+            //print( fac );
+            bool isInSQ = fac < fallOff_rad_sq;
+            fac = falloff_radius - sqrt( fac );
+            fac /= falloff_radius;
+            if ( //falloff_type == eWarp_Falloff_None || (
+                        isInSQ && fac != 0.0f )
+            //)
+            {
+                // vert_groupがあるなら
+                // if( defgrp_index != -1)
+                // 0.8 ~ 0.9
+                //print( fac );
+                var fallOff = CulcFall(fac);
+                fallOff *= weight;
+                //if(tex_co)
+                if ( fallOff != 0.0f )
+                {
+                    // 280付近
+                    co = mat_from_inv.MultiplyPoint( co );
+                    // fromの空間に持っていく
+                    if ( fallOff == 1.0f )
+                    //(fallOff >0.0f)
+                    {
+                        co = mat_final.MultiplyPoint( co );
+                    }
+                    else
+                    {
+                        if ( flag.HasFlag( WarpFlag.MOD_WARP_VOLUME_PRESERVE ) )
+                        {
+                            /* interpolate the matrix for nicer locations */
+                            var mat_unit = Matrix.identity;
+
+                            tmat = blend_m4_m4m4( mat_unit , mat_final , fallOff );
+                            co = tmat.MultiplyPoint( co );
+                        }
+                        else
+                        {
+                            var tvec = mat_final.MultiplyPoint( co );
+                            var newco = Vector3.Lerp( co , tvec , fallOff );
+                            //var writeMat = "mat_final\n" + mat_final.GetPosition();
+                            //RarePrint( $"{tvec} co : {co} newco : {newco} fallOff : {fallOff}" );
+                            co = newco;
+                        }
+                    }
+
+                    /* out of the 'from' objects space */
+                    co = mat_from.MultiplyPoint( co );
+                    //RarePrint( $"oriCo {oriCo} ; co {co}" );
+                } // if(fallOff != 0.0f)
+            } // if( falloff_type == eWarp_Falloff_None || (isInSQ && fac != 0.0f) )
+            temp.Add( co );
+        } // for
+        return temp;
+    }
+
+    // Start is called before the first frame update
+    void Start( )
+    {
+        mesh = 
+            GetComponent<SkinnedMeshRenderer>( ).sharedMesh;
+
+        OrgVertice = new Vector3[ mesh.vertices.Length ];
+        System.Array.Copy( mesh.vertices , OrgVertice , mesh.vertices.Length );
+
+        var numVerts = mesh.vertices.Length;
+        for ( int i = 0 ; i < numVerts ; i++ )
+        {
+            //OrgVertice[ i ] = mesh.vertices[ i ];
+            //SpawnCube( mesh.vertices[ i ] );
+        }
+    }
+    private void OnDestroy( )
+    {
+        System.Array.Copy( OrgVertice , mesh.vertices , mesh.vertices.Length );
+        mesh.SetVertices( OrgVertice );
+        
+    }
+
+    List<Vector3> CreateVert( List<Vector3> v )
+    {
+        var temp = new List<Vector3>(v.Count);
+        for ( int i = 0 ; i < v.Count ; i++ )
+        {
+            temp.Add( v[ i ] + Random.insideUnitSphere );
+        }
+        return temp;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate( )
+    {
+
+        mesh.SetVertices(warpModifier_do( ) );
+
+        //var numVerts = mesh.vertices.Length;
+        //List<Vector3> temp =new List<Vector3>();
+
+        //mesh.GetVertices( temp );
+        //    mesh.SetVertices( CreateVert( temp ));
+
+    }
+}
---
+++ Assets/Util/EntityBase.cs
@@ -0,0 +1,158 @@
+﻿//using Optional;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unit =
+    //UnityEngine.GameObject;
+    EntityBase;
+using Gobj = UnityEngine.GameObject;
+using V3 = UnityEngine.Vector3;
+public class EntityBase : MonoBehaviour
+{
+    //public abstract void OnJustClick( HandBoneController controller);
+    // マウスクリック反応を実装するなら
+    public virtual void OnJustClick( ) { }
+
+    public static string __Function( )
+    {
+#if DEBUG
+        var stackTrace = new System.Diagnostics.StackTrace();
+        return stackTrace.GetFrame( 1 ).GetMethod( ).Name;
+#else
+        return "";
+#endif
+    }
+    public static Unit SpawnCube( V3 pos )
+    {
+        var obj = Gobj.CreatePrimitive(PrimitiveType.Cube);
+        obj.transform.position = pos;
+        var p = obj.transform.position;
+        //obj.transform.position = new V3( p.x , p.y , CameraOffset);
+        obj.transform.localScale = new V3( 0.1f , 0.1f , 0.1f);
+        var unit = obj.AddComponent<Unit>( );
+        return unit;
+    }
+
+    //public void SetTransparent()
+    //{
+
+    //    var mayMat = GetMaterial( );
+    //    var trans = Shader.Find("UI/Lit/Transparent");
+    //    mayMat.Match( m => m.shader = trans , () => Debug.Log("shader not found"));
+    //}
+
+    //public Option<Material> GetMaterial()
+    //{
+    //    var renderer = GetComponent<MeshRenderer>( );
+    //    if ( renderer )
+    //    {
+
+    //        return renderer.material.Some();
+    //    }
+    //    var skinRend = GetComponent<SkinnedMeshRenderer>();
+    //    if(skinRend)
+    //    {
+    //        return skinRend.material.Some();
+    //    }
+    //    return Option.None<Material>( );
+    //}
+
+    public void SetColor(Color color)
+    {
+        var renderer = GetComponent<MeshRenderer>( );
+        if ( renderer )
+        {
+            renderer.material.color = color;
+            return;
+        }
+        var skinRend = GetComponent<SkinnedMeshRenderer>();
+        if(skinRend)
+        {
+            skinRend.material.color = color;
+        }
+    }
+
+    public static EntityBase SetEntity(GameObject gobj)
+    {
+        var ent = gobj.AddComponent<EntityBase>( );
+        return ent;
+    }
+
+    public void Show()
+    {
+        SetActive( true );
+    }
+
+    public void Hide()
+    {
+        SetActive( false );
+    }
+
+    public void SetActive(bool isActive)
+    {
+        gameObject.SetActive( isActive );
+    }
+
+    public void SetParent(EntityBase parent, bool stay)
+    {
+        transform.SetParent( parent.transform , stay );
+    }
+
+    public Vector3 GetPos()
+    {
+        return transform.position;
+    }
+
+    public Quaternion GetRot()
+    {
+        return transform.rotation;
+    }
+
+    public Vector3 GetScale()
+    {
+        return transform.localScale;
+    }
+
+    public void SetPos(Vector3 pos)
+    {
+        transform.position = pos;
+    }
+
+    public void SetOffset(float x , float y = 0 , float z = 0)
+    {
+        var pos = GetPos();
+        SetPos( new Vector3(pos.x + x , pos.y + y , pos.z + z ) );
+    }
+
+    public void SetRot(Quaternion rot)
+    {
+        transform.rotation = rot;
+    }
+
+    public void SetScale(Vector3 scale)
+    {
+        transform.localScale = scale;
+    }
+
+    public void SetUnitScale(float x )
+    {
+        transform.localScale = new Vector3( x , x , x );
+    }
+
+    public static bool IsRange<T>(List<T> self , int index)
+    {
+        return 0 <= index && index < self.Count;
+    }
+
+    public void Log(object log)
+    {
+        Debug.Log( name + " , " + log );
+    }
+
+    public void Warn(object log)
+    {
+        Debug.LogWarning( name + " , " + log );
+    }
+
+}
